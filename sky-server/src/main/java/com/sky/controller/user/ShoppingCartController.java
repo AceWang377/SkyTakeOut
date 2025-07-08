3aca682b85f23c983197f32936042791650df266
Zixuan Wang <zxw377@student.bham.ac.uk>
2025-07-08 22:16:57 +0100

    add items to cart and show cart list develop code

---
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -0,0 +1,52 @@
+package com.sky.controller.user;
+
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import com.sky.result.Result;
+import com.sky.service.ShoppingCartService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.nullness.compatqual.PolyNullType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user/shoppingCart")
+@Api(tags = "C端-购物车接口")
+@Slf4j
+public class ShoppingCartController {
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
+    /**
+     * 添加购物车
+     *
+     * @param shoppingCartDTO
+     * @return
+     */
+    @PostMapping("/add")
+    @ApiOperation("Add to cart")
+    public Result add(@RequestBody ShoppingCartDTO shoppingCartDTO) {
+        log.info("添加购物车:{}", shoppingCartDTO);
+        shoppingCartService.add(shoppingCartDTO);
+        return Result.success();
+    }
+
+    /* *
+     * 查看购物车
+     */
+    @GetMapping("/list")
+    @ApiOperation("View Cart")
+    public Result<List<ShoppingCart>> list() {
+        log.info("查看购物车");
+        List<ShoppingCart> shoppingCart = shoppingCartService.showShoppingCart();
+        return Result.success(shoppingCart);
+    }
+
+
+}
